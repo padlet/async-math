0a2b4e705a75d5cace43d9f917124e11af427ab5
Nitesh <nitesh@padlet.com>
2017-09-16 02:41:09 -0700

    Async math functions with tests.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # async-math
 Npm package for async math operations.
+
+## Introduction
+Stop writing archaic, boring code.
+Why use native operators when you can use an npm package?  
+Why use synchronous arithmetic when you can do so asynchronously?  
+Why get guaranteed results when you can get the promise of one? 
+
+## Usage
+
+### Adding two numbers
+```javascript
+import { add } from "async-math"
+add(8, 4).then(function(sum) { console.log(sum) })
+```
+Console output should be 12
+
+### Subtracting a number from another
+```javascript
+import { subtract } from "async-math"
+subtract(8, 4).then(function(difference) { console.log(difference) })
+```
+Console output should be 4
+
+### Multiplying two numbers
+```javascript
+import { multiply } from "async-math"
+multiply(8, 4).then(function(product) { console.log(product) })
+```
+Console output should be 32
+
+### Divide a number from another
+```javascript
+import { divide } from "async-math"
+divide(8, 4).then(function(quotient) { console.log(quotient) })
+```
+Console output should be 2
---
+++ index.js
@@ -0,0 +1,55 @@
+let add = function (num1, num2) {
+  return new Promise((resolve, reject) => {
+    try {
+      const sum = num1 + num2;
+      setTimeout(function () { resolve(sum); }, 0);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+let subtract = function (num1, num2) {
+  return new Promise((resolve, reject) => {
+    try {
+      const difference = num1 - num2;
+      setTimeout(function () { resolve(difference); }, 0);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+let multiply = function (num1, num2) {
+  return new Promise((resolve, reject) => {
+    try {
+      const product = num1 * num2;
+      setTimeout(function () { resolve(product); }, 0);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+let divide = function (num1, num2) {
+  return new Promise((resolve, reject) => {
+    try {
+      const quotient = num1 / num2;
+      setTimeout(function () { resolve(quotient); }, 0);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+let shootMe = function () {
+  return new Promise((resolve, reject) => {
+    try {
+      setTimeout(() => { resolve("bang"); }, 0);
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+export { add, subtract, multiply, divide, shootMe };
